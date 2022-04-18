1e6a4442ce8f16b40ae9671915cb7fee0bda350d
claudiu.vlasceanu <claudiu.vlasceanu@nttdata.com>
2022-04-18 12:07:14 -0700

    Adobe tests

---
+++ AutomatedTests/src/main/java/ca/bc/gov/open/ccd/test/AdobeTestRunner.java
@@ -0,0 +1,12 @@
+package ca.bc.gov.open.ccd.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdobeTestRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdobeTestRunner.class, args);
+    }
+}
